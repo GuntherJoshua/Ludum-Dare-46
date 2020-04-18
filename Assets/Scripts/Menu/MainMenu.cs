877f90faed477dd8ac3cfc02adc7a38243585726
0ffz <dan.voznyy@gmail.com>
2020-04-18 17:23:04 -0400

    Adding main menu and assets required for it

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu {
+    public class MainMenu : MonoBehaviour {
+        public void StartGame() {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void QuitGame() {
+            Application.Quit();
+        }
+    }
+}
