53f352a457c62732d6df06134f7de7b1dae6d40e
0ffz <dan.voznyy@gmail.com>
2020-04-18 12:17:18 -0400

    Add fan animation, put .idea in gitignore

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mover : MonoBehaviour
-{
+public class Mover : MonoBehaviour {
     public float trackLength;
-    public enum direction
-    {
+
+    public enum direction {
         Horizontal,
         Vertical
     };
+
     public direction dir;
     Vector3 relativePosition;
     Vector3 intialPosition;
@@ -18,43 +18,38 @@ public class Mover : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         intialPosition = transform.position;
         lr = GetComponentInChildren<LineRenderer>();
         cam = Camera.main;
-        if (dir == direction.Vertical)
-        {
+        if (dir == direction.Vertical) {
             lr.SetPosition(0, (Vector3.up * -trackLength) + transform.position);
             lr.SetPosition(1, (Vector3.up * trackLength) + transform.position);
         }
-        else
-        {
+        else {
             lr.SetPosition(0, (Vector3.right * -trackLength) + transform.position);
             lr.SetPosition(1, (Vector3.right * trackLength) + transform.position);
         }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
+    void Update() {
     }
 
-    private void OnMouseDown()
-    {
+    private void OnMouseDown() {
         relativePosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
-    private void OnMouseDrag()
-    {
+    private void OnMouseDrag() {
         var mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        if ( dir == direction.Horizontal){
-            transform.position = new Vector3(Mathf.Clamp(mousePos.x, -trackLength + intialPosition.x, trackLength + intialPosition.x), intialPosition.y) - relativePosition;
+        if (dir == direction.Horizontal) {
+            transform.position =
+                new Vector3(Mathf.Clamp(mousePos.x, -trackLength + intialPosition.x, trackLength + intialPosition.x),
+                    intialPosition.y) - relativePosition;
         }
-        else
-        {
-            transform.position = new Vector3(intialPosition.x , Mathf.Clamp(mousePos.y, -trackLength + intialPosition.y, trackLength + intialPosition.y));
+        else {
+            transform.position = new Vector3(intialPosition.x,
+                Mathf.Clamp(mousePos.y, -trackLength + intialPosition.y, trackLength + intialPosition.y));
         }
     }
 }
