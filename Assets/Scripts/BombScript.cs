bd6656340e70a451bb61c21d05938bc6c796bfe9
Cosmo48 <59401027+Cosmo48@users.noreply.github.com>
2020-04-18 18:20:47 -0400

    Cleaned Up Code

--- Assets/Scripts/BombScript.cs
+++ Assets/Scripts/BombScript.cs
@@ -1,23 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BombScript : MonoBehaviour
 {
     public float explodeRadius;
     public float explodeSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnMouseDown() {
         Explode();
