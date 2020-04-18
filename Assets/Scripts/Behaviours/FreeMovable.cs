1bf67031b0c27a6d6c094892b6aab3aa8645f01a
Cosmo48 <59401027+Cosmo48@users.noreply.github.com>
2020-04-18 15:56:06 -0400

    Added Freemovable attachment to GameItems

---
+++ Assets/Scripts/Behaviours/FreeMovable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeMovable : GameItem
+{
+
+    Camera cam;
+    Vector3 relativePos;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+
+    private void OnMouseDown() {
+        if (!Allowed) return;
+        relativePos = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDrag() {
+        if (!Allowed) return;
+        Debug.Log(true);
+        var mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos + relativePos;
+    }
+}
