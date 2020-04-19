a68ac9e0899d604e8f6d8695d0b9a5d5828d4717
Cosmo48 <59401027+Cosmo48@users.noreply.github.com>
2020-04-18 21:39:35 -0400

    Added Trigger and particle effects

---
+++ Assets/ParticleScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour
+{
+    ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        ps = GetComponent<ParticleSystem>();
+        ps.Play();
+        Invoke("Remove", 1f);
+    }
+
+
+    void Remove() {
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/EndBoxScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EndBoxScript : MonoBehaviour
+{
+
+    public GameObject explosion;
+    public static int rocksEntered;
+
+    private void Start() {
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other);
+
+        if (other.gameObject.layer == 8) {
+            rocksEntered++;
+        }
+        SpriteRenderer sr = other.gameObject.GetComponent<SpriteRenderer>();
+
+        var newExplosion = explosion;
+        ParticleSystem ps = newExplosion.GetComponent<ParticleSystem>();
+        var col = ps.colorOverLifetime;
+        Gradient grad = new Gradient();
+        grad.SetKeys(new GradientColorKey[] { new GradientColorKey(sr.color, 0.0f), new GradientColorKey(Color.white, 1.0f) }, new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(0.0f, 1.0f) });
+        col.color = grad;
+        Instantiate(newExplosion, other.gameObject.transform);
+        other.attachedRigidbody.Sleep();
+        Destroy(other);
+    }
+}
